7cf61bd5fbefed94dcd0002018ce5af9d60d6cd9
Affan Pathan <pathan.affan02@gmail.com>
2024-06-22 13:04:24 +0530

    added flask monitoring dashboard

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import flask_monitoringdashboard as dashboard
+
 
 # Create Flask application instance
 app = Flask(__name__)
+dashboard.bind(app)
+
 
 # Load configuration from config.py
 app.config.from_pyfile('../config.py')
